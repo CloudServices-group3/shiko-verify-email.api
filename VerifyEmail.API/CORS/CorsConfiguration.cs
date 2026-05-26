0e100154029b5c06a26bc1e7a655ebcbcf768661
AhlmanJ <johan.ahlman@hotmail.com>
2026-05-26 02:49:13 +0200

    Add URL to CORS configuration.

--- VerifyEmail.API/CORS/CorsConfiguration.cs
+++ VerifyEmail.API/CORS/CorsConfiguration.cs
@@ -9,7 +9,7 @@ public static class CorsConfiguration
             options.AddPolicy("Frontend", policy =>
             {
                 policy
-                    .WithOrigins("http://localhost:3000", "https://localhost:3000")
+                    .WithOrigins("http://localhost:3000", "https://localhost:3000", "https://shiko-email-verification-api.azurewebsites.net")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
